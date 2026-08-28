bc05973c5e13491c33fd5e56320d23e27c2d983c
gawain <lixing@2023>
2026-08-28 22:33:39 +0800

    构建报错

--- apps/web/layouts/default.vue
+++ apps/web/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen flex flex-col">
     <AppHeader />
-    修改了
+    修改了zagengxin 
     <main class="flex-1 w-full max-w-7xl mx-auto px-4 sm:px-6 py-6">
       <slot />
     </main>
@@ -9,3 +9,5 @@
     <BaseToast />
   </div>
 </template>
+docker login --username=aliyun@063574181 crpi-uo2gx7e52udrmw75.cn-guangzhou.personal.cr.aliyuncs.com
+docker login --username=aliyun0063574181 crpi-uo2gx7e52udrmw75.cn-guangzhou.personal.cr.aliyuncs.com
