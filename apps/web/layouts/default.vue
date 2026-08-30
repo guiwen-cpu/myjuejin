7fb5071b2ed740a08b0cb364480cb289ebb33290
gawain <lixing@2023>
2026-08-30 15:59:14 +0800

    1

--- apps/web/layouts/default.vue
+++ apps/web/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="min-h-screen flex flex-col">
-    我是生产环境
+    我是生产环境11
     <AppHeader />
     <main class="flex-1 w-full max-w-7xl mx-auto px-4 sm:px-6 py-6">
       <slot />
